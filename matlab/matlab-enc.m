4493f8f991179af06eaa576bb292d42ab4a36342
Andy P. <andy@pollari.ch>
2014-12-27 16:24:06 +0100

    symmetrische Verschl. performance

--- matlab/matlab-enc.m
+++ matlab/matlab-enc.m
@@ -1,35 +1,60 @@
 % Declare image to load
-% read in tiff image
-total_runtime = tic;
-my_image = imread('ff.bmp','bmp');
-my_image_size = size(my_image)
-y = my_image_size(1)
-x = my_image_size(2)
-z = my_image_size(3)
 
-% generate random matrix
-% randomMatrix = randi([0,255], 84, 220, 3);
-randomMatrix = randi([0,255], y, x, z);
-randomMatrix = uint8(randomMatrix);
+files = dir('images/large-2.jpg');
+% files = dir('images/*.jpg');
 
-% encrypt image
-image_encrypted = my_image;
-enc_time = tic;
-image_encrypted = encData(my_image, randomMatrix);
-enc_time = toc(enc_time)
-image_decrypted = encData(image_encrypted, randomMatrix);
+% initialize matrixes for report
+time = [];
+imgName = [];
+width = [];
+height = [];
+megapixel = [];
 
-total_runtime = toc(total_runtime)
+for file = files'
+    filename = file.name
 
-figure()
-subplot(1,3,1)
-imshow(my_image,[])
-title('original image')
-subplot(1,3,2)
+    % read image
+    imagePath = strcat('images/', filename);
+    total_runtime = tic;
+    my_image = imread(imagePath, 'jpg');
+    my_image_size = size(my_image);
+    y = my_image_size(1);
+    x = my_image_size(2);
+    z = my_image_size(3);
 
-imshow(image_encrypted,[])
-title('Encrypted image')
-subplot(1,3,3)
+    % generate random matrix
+    randomMatrix = randi([0,255], y, x, z);
+    randomMatrix = uint8(randomMatrix);
 
-imshow(image_decrypted,[])
-title('Decrypted Image')
+    % encrypt image
+    image_encrypted = my_image;
+    enc_time = tic;
+    image_encrypted = encData(my_image, randomMatrix);
+    enc_time = toc(enc_time);
+    % image_decrypted = encData(image_encrypted, randomMatrix);
+
+    % create statistics for the table
+    time = cat(1, time, [enc_time]); % time in seconds
+    width = cat(1, width, [x]);
+    height = cat(1, height, [y]);
+    megapixel = cat(1, megapixel, [(x*y)/10^6]);
+    imgName = [imgName; {filename}];
+
+    %figure()
+    subplot(1,3,1)
+    imshow(my_image,[])
+    title('Original image')
+    subplot(1,3,2)
+
+    imshow(image_encrypted,[])
+    title('Encrypted image')
+    % subplot(1,3,3)
+
+    %imshow(image_decrypted,[])
+    %title('Decrypted Image')
+
+end
+
+T = table(imgName,width, height,megapixel, time);
+writetable(T,'performance-symmetric-enc.csv');
+
